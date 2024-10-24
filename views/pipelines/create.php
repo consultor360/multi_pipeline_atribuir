bfd2b56509766c716843443ecbe22ee86488e2b3
Neto Menezes <61053569+consultor360@users.noreply.github.com>
2024-10-24 02:20:40 -0300

    Add files via upload

--- views/pipelines/create.php
+++ views/pipelines/create.php
@@ -3,6 +3,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
 <div id="wrapper">
     <div class="content">
         <div class="row">
@@ -25,35 +26,37 @@ defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
                         <!-- Adicionando os campos de seleção de membros e funções -->
                         <?php
-// Carregar membros da equipe e funções
-$staff_and_roles = $this->multi_pipeline_model->get_staff_and_roles();
-?>
+                        // Carregar membros da equipe e funções
+                        $staff_and_roles = $this->multi_pipeline_model->get_staff_and_roles();
+                        ?>
 
-<div class="form-group">
-    <label for="staff_ids">Atribuir Pipeline a Usuarios</label>
-    <select name="staff_ids[]" class="form-control" multiple>
-        <?php foreach ($staff_and_roles['staff'] as $staff): ?>
-            <option value="<?php echo $staff['staffid']; ?>"><?php echo $staff['firstname'] . ' ' . $staff['lastname']; ?></option>
-        <?php endforeach; ?>
-    </select>
-</div>
+                        <div class="form-group">
+                        <div class="col-md-6">
+                            <label for="staff_ids">Atribuir Pipeline a Usuários</label>
+                            <select name="staff_ids[]" class="form-control selectpicker" multiple data-live-search="true">
+                                <?php foreach ($staff_and_roles['staff'] as $staff): ?>
+                                    <option value="<?php echo $staff['staffid']; ?>"><?php echo $staff['firstname'] . ' ' . $staff['lastname']; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
 
-<div class="form-group">
-    <label for="role_ids">Atribuir Pipeline a Funções</label>
-    <select name="role_ids[]" class="form-control" multiple>
-        <?php foreach ($staff_and_roles['roles'] as $role): ?>
-            <option value="<?php echo $role['roleid']; ?>"><?php echo $role['name']; ?></option>
-        <?php endforeach; ?>
-    </select>
-</div>
+                        <div class="form-group">
+                        <div class="col-md-6">
+                            <label for="role_ids">Atribuir Pipeline a Funções</label>
+                            <select name="role_ids[]" class="form-control selectpicker" multiple data-live-search="true">
+                                <?php foreach ($staff_and_roles['roles'] as $role): ?>
+                                    <option value="<?php echo $role['roleid']; ?>"><?php echo $role['name']; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
 
                         <div class="row">
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-primary pull-right">
-                                    <?php echo _l('submit'); ?>
-                                </button>
-                            </div>
-                        </div>
+    <div class="col-md-12" style="margin-top: 20px;"> <!-- Adicionado margin-top aqui -->
+        <button type="submit" class="btn btn-primary pull-right">
+            <?php echo _l('submit'); ?>
+        </button>
+    </div>
+</div>
                         <?php echo form_close(); ?>
                     </div>
                 </div>
@@ -62,6 +65,7 @@ $staff_and_roles = $this->multi_pipeline_model->get_staff_and_roles();
     </div>
 </div>
 <?php init_tail(); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
 <script>
 $(function() {
     appValidateForm($('form'), {
